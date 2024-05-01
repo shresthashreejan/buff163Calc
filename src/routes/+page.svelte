63184249ef09d8eca9b1c37cd4de541e1e972ac8
shreejan shrestha <shr.shreejan@gmail.com>
2024-05-01 12:25:51 +0545

    Updated API

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -54,7 +54,7 @@
 
 				try {
 					const response = await fetch(
-						'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/cny.json'
+						'https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/cny.json'
 					);
 
 					if (!response.ok) {
@@ -72,7 +72,7 @@
 
 					const modal = document.getElementById('modal');
 					modal.showModal();
-				} catch {
+				} catch (error) {
 					console.error('Error fetching data:', error);
 				}
 			};
